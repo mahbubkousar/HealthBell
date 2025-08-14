52cd1e85714422401c39a26aa8383e55437c3118
ahnaftahmid19 <2221054@iub.edu.bd>
2025-08-14 22:39:29 +0600

    firebase config updated

--- firebase-config.js
+++ firebase-config.js
@@ -6,13 +6,13 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-
 // Your web app's Firebase configuration
 // PASTE YOUR FIREBASE CONFIG OBJECT HERE
 const firebaseConfig = {
-  apiKey: "AIzaSyAogYrFUa9lvmarI3Mv02OWIrUaHuTflYA",
-  authDomain: "healthbell-74b9f.firebaseapp.com",
-  projectId: "healthbell-74b9f",
-  storageBucket: "healthbell-74b9f.firebasestorage.app",
-  messagingSenderId: "601048053152",
-  appId: "1:601048053152:web:cf752e14f63df168c620c5",
-  measurementId: "G-RWJQD6VTQV"
+	apiKey: "AIzaSyCPco6NYzNQ0wLTHCKh2i9R_YxDtwQNm3M",
+	authDomain: "healthbell2-f72f7.firebaseapp.com",
+	projectId: "healthbell2-f72f7",
+	storageBucket: "healthbell2-f72f7.firebasestorage.app",
+	messagingSenderId: "367462999886",
+	appId: "1:367462999886:web:bb8381fc2a58b1971a201f",
+	measurementId: "G-QW48EMZCQV",
 };
 
 // Initialize Firebase
